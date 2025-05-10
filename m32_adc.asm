947df036bb208ad9c562a3ccc5200fa80bf52be6
anti_botinok <steppp1518@gmail.com>
2025-05-10 20:41:36 +0300

    add acp full num output

--- m32_adc.asm
+++ m32_adc.asm
@@ -6,6 +6,8 @@
 .DEF CURRENT_INDICATOR = r19
 .DEF PARAM_0 = r20
 .DEF PARAM_1 = r21
+.DEF PARAM_3 = r24
+.DEF PARAM_4 = r25
 .DEF POWER_0 = r22
 .DEF POWER_1 = r23
 
@@ -28,37 +30,47 @@ RESET:
 	rjmp main
 	
 main:
+	;ADC!!!
+	ldi TEMP, 199
+	out ADCSRA, TEMP
+SysDoLoop_S2:
+	sbic	ADCSRA,ADSC
+	rjmp	SysDoLoop_S2
+
+	in POWER_0, ADCL
+	in POWER_1, ADCH
+
 	mov PARAM_0, POWER_0
 	mov PARAM_1, POWER_1
-	rcall mod10_16bit
+	rcall Out_Number_On_Board
 	
-	;ADC!!!
-	;ldi TEMP, 199
-	;out ADCSRA, TEMP
-;SysDoLoop_S2:
-	;sbic	ADCSRA,ADSC
-	;rjmp	SysDoLoop_S2
-
-	;in NUMBER, ADCL
-	call mod10_16bit
+	rjmp main
+	
+;args PARAM_1;PARAM_0
+Out_Number_On_Board:
+	ldi CURRENT_INDICATOR, 1 << 3
+Out_Number_On_Board_Loop:
+	rcall mod10_16bit
 	mov NUMBER, PARAM_0
+	mov PARAM_0, PARAM_3
+	mov PARAM_1, PARAM_4
 			
 	rcall Out_Light
-	rcall wait
 	
-	lsl CURRENT_INDICATOR
-	ldi TEMP, 0XF
-	and CURRENT_INDICATOR, TEMP
+	lsr CURRENT_INDICATOR
 	
 	ldi TEMP, 0
 	cp CURRENT_INDICATOR, TEMP
-	breq Init_indicator
-
-	rjmp main
+	in TEMP, SREG
+	lsr TEMP
+	andi TEMP, 1
+	sbrs TEMP, 0
 	
-Init_indicator:
-	ldi CURRENT_INDICATOR, 1
+	rjmp Out_Number_On_Board_Loop
+End_Loop:
+	ret
 	
+;args NUMBER, CURRENT_INDICATOR
 Out_Light:
 	out PORTD, CURRENT_INDICATOR
 	mov TEMP, ZL
@@ -67,27 +79,60 @@ Out_Light:
 	mov ZL, TEMP
     out PORTB, LIGHT
 
+	rcall wait
+	ret
+
+;args PARAM_1;PARAM_0
+;return PARAM_0 = mod 10
+;return PARAM_4;PARAM_3 = PARAM_1;PARAM_0 // 10
 mod10_16bit:
-check_param_0:
+	clr PARAM_3
+	clr PARAM_4
+
+	push r18
+	push r19
+check:
 	ldi TEMP, 10
 	cp PARAM_0, TEMP
-	brlo check_param_1
+	in r18, SREG
+	andi r18, 0x01
+	
+	ldi TEMP, 0
+	cp PARAM_1, TEMP
+	in r19, SREG
+	lsr r19
+	andi r19, 0x01
+	
+	and r18, r19
+	sbrc r18, 0
+	rjmp mod10_done
+	
+	clr r18
+	clr r19
 mod10_loop:
+	;16 bit num -= 10
 	ldi TEMP, 10
     sub PARAM_0, TEMP
 	clr TEMP
     sbc PARAM_1, TEMP
 
-	rjmp check_param_0
-check_param_1:
+	;param1 = PARAM_4;PARAM_3
+	;param1 = 16 bit num // 10
+	;param1 += 1
+	ldi TEMP, 1
+	add PARAM_3, TEMP
 	ldi TEMP, 0
-	cp PARAM_1, TEMP
-	breq mod10_done
-	rjmp mod10_loop
+	adc PARAM_4, TEMP
+
+	rjmp check
 mod10_done:
+	pop r19
+	pop r18
     ret
 
 wait:
+	push r20
+	push r21
     ldi r20, 100
 w1:
     ldi r21, 250
@@ -97,6 +142,9 @@ w2:
     brne w2
     dec r20
     brne w1
+	
+	pop r21
+	pop r20 
     ret
 	
 INITSYS:
